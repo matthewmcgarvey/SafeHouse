fa3ae7d1fa1867e5fe0701b6bfb9def9b1014b45
tnraptor14 <tnraptor14@hotmail.com>
2017-04-05 18:59:21 -0500

    init

---
+++ src/main/java/com/theironyard/SafeHouseApplication.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeHouseApplication.class, args);
+	}
+}
