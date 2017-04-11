537b47aa2c20d404d49c1c3dcc938245fbe5e9ed
tnraptor14 <tnraptor14@hotmail.com>
2017-04-11 14:00:20 -0500

    implemented recall api

---
+++ src/main/java/com/theironyard/API/recalls/Recall.java
@@ -0,0 +1,71 @@
+package com.theironyard.API.recalls;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+public class Recall {
+    public Integer recallNumber;
+    public String recallURL;
+    public String recallTitle;
+    public String recallDescription;
+    public String productName;
+    public String productDescription;
+    public String productModel;
+    public String UPC;
+
+    private String toJson = "format=json";
+
+    private StringBuffer url = new StringBuffer("https://www.saferproducts.gov/RestWebServices/Recall?");
+
+    public Map<String, Object>[] submit() {
+        if (recallNumber != null) {
+            url.append("RecallNumber=");
+            url.append(recallNumber);
+            url.append("&");
+        }
+        if (recallURL != null) {
+            url.append("RecallUrl=");
+            url.append(recallURL);
+            url.append("&");
+        }
+        if (recallTitle != null) {
+            url.append("RecallTitle=");
+            url.append(recallTitle);
+            url.append("&");
+        }
+        if (recallDescription != null) {
+            url.append("RecallDescription=");
+            url.append(recallDescription);
+            url.append("&");
+        }
+        if (productName != null) {
+            url.append("ProductName=");
+            url.append(productName);
+            url.append("&");
+        }
+        if (productDescription != null) {
+            url.append("ProductDescription=");
+            url.append(productDescription);
+            url.append("&");
+        }
+        if (productModel != null) {
+            url.append("ProductModel=");
+            url.append(productModel);
+            url.append("&");
+        }
+        if (UPC != null) {
+            url.append("UPC=");
+            url.append(UPC);
+            url.append("&");
+        }
+
+        url.append(toJson);
+        String recallUrl = url.toString();
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Map[]> response = restTemplate.getForEntity(recallUrl, Map[].class);
+        return response.getBody();
+    }
+
+}
