b4b5ed720e28599f6277d3dc43ec45ebafad3123
tnraptor14 <tnraptor14@hotmail.com>
2017-04-13 15:23:01 -0500

    removed unneeded file

--- src/main/java/com/theironyard/SafeHouseApplication.java
+++ src/main/java/com/theironyard/SafeHouseApplication.java
@@ -1,6 +1,5 @@
 package com.theironyard;
 
-import com.theironyard.api.AmazonUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SafeHouseApplication {
 
     public static void main(String[] args) {
-//        System.out.println(AmazonUtil.lookupItem("Samsung Galaxy", "All"));
         SpringApplication.run(SafeHouseApplication.class, args);
     }
 }
