4eda9efa4cd57746433748fa6139f6daf06e1f8e
tnraptor14 <tnraptor14@hotmail.com>
2017-04-13 17:03:14 -0500

    working cors

--- src/main/java/com/theironyard/security/WebSecurityConfig.java
+++ src/main/java/com/theironyard/security/WebSecurityConfig.java
@@ -35,10 +35,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers(HttpMethod.POST, "/users").permitAll()
                 // Allow login without being logged in
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.OPTIONS, "/login").permitAll()
                 // Allow item search without being logged in
                 .antMatchers(HttpMethod.GET, "/items/**/**").permitAll()
                 // All other requests must be authenticated
                 .anyRequest().authenticated().and()
+                // Allow CORS
+                .cors().and()
                 // Adds login route
                 .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),
                         UsernamePasswordAuthenticationFilter.class)
