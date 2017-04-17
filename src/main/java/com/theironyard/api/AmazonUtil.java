613e28090d98ff34be16328069b084981cbe0405
B-Maxwell <MacbookStudioPro@Macbook-Studio-Pro.local>
2017-04-17 14:59:59 -0500

    Updated Amzn API - Code Cleanse

--- src/main/java/com/theironyard/api/AmazonUtil.java
+++ src/main/java/com/theironyard/api/AmazonUtil.java
@@ -1,19 +1,11 @@
 package com.theironyard.api;
 
-import com.theironyard.entities.SearchItem;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.XML;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class AmazonUtil {
@@ -44,90 +36,7 @@ public class AmazonUtil {
         params.put("Keywords", keywords); //Keyword(s) entered by user
         params.put("AssociateTag", "safe0cd-20");
 
-//        try {
-//
-//            URL url = new URL(helper.sign(params));
-//            BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
-//            String strTemp;
-//
-//            while (null != (strTemp = br.readLine())) {
-//                JSONObject xmlJSONObj = XML.toJSONObject(strTemp);
-//                JSONArray itemArray = xmlJSONObj.getJSONObject("ItemSearchResponse")
-//                        .getJSONObject("Items")
-//                        .getJSONArray("Item");
-//
-//                List<SearchItem> searchItemResults = new ArrayList<>();
-//
-//                for (int i = 0; i < itemArray.length(); i++) {
-//
-//                    JSONObject item = (JSONObject) itemArray.get(i);
-//                    String brand;
-//                    String model;
-//                    BigInteger upc;
-//                    String imageUrl;
-//                    String asin;
-//
-//                    String title = item.getJSONObject("ItemAttributes").getString("Title");
-//
-//                    try {
-//                        asin = item.getString("ASIN");
-//                    } catch (Exception asinTypeInt) {
-//                        Integer asinInt = item.getInt("ASIN");
-//                        asin = asinInt.toString();
-//                    }
-//
-//
-//                    try {
-//                        brand = item.getJSONObject("ItemAttributes").getString("Brand");
-//                    } catch (Exception noBrand) {
-//                        brand = "Brand not available";
-//                    }
-//
-//                    try {
-//                        model = item.getJSONObject("ItemAttributes").getString("Model");
-//                    } catch (Exception noModel) {
-//                        model = "Model not available";
-//                    }
-//
-//                    try {
-//                        upc = item.getJSONObject("ItemAttributes").getBigInteger("UPC");
-//                    } catch (Exception noUpc) {
-//                        upc = BigInteger.valueOf(0);
-//                    }
-//
-//                    try {
-//                        imageUrl = item.getJSONObject("LargeImage").getString("URL");
-//                    } catch (Exception noLrgImg) {
-//                        try {
-//                            imageUrl = item.getJSONObject("ImageSets")
-//                                    .getJSONObject("ImageSet")
-//                                    .getJSONObject("LargeImage")
-//                                    .getString("URL");
-//
-//                        } catch (Exception noImgSetObject) {
-//                            imageUrl = item.getJSONObject("ImageSets")
-//                                    .getJSONArray("ImageSet")
-//                                    .getJSONObject(0)
-//                                    .getJSONObject("LargeImage")
-//                                    .getString("URL");
-//                        }
-//                    }
-//
-//                    SearchItem searchItem = new SearchItem(title, brand , model, upc, imageUrl, asin);
-//                    searchItemResults.add(searchItem);
-//                }
-//
-//                return new ResponseEntity<>(searchItemResults, HttpStatus.OK);
-//            }
-//
-//        } catch (Exception ex) {
-//
-//            ex.printStackTrace();
-//            return new ResponseEntity<>("Problem with the search request", HttpStatus.BAD_REQUEST);
-//        }
-//
-//        return new ResponseEntity<>("An unexpected error occurred", HttpStatus.EXPECTATION_FAILED);
-//    }
+
         try {
             URL url = new URL(helper.sign(params));
 
