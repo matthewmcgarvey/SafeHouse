65e999ec1ef6875b9bbbc8123edc14ff628987d0
tnraptor14 <tnraptor14@hotmail.com>
2017-04-19 16:49:01 -0500

    added recallscontroller

--- src/main/java/com/theironyard/controllers/SafeHouseController.java
+++ src/main/java/com/theironyard/controllers/SafeHouseController.java
@@ -11,9 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class SafeHouseController {
 
-    @Autowired
-    private Items items;
-
     // Search Amazon Product API
     @RequestMapping(path = "/items/{category}/{keywords}/{page}", method = RequestMethod.GET)
     public ResponseEntity<?> searchItems(@PathVariable String keywords,
@@ -24,12 +21,5 @@ public class SafeHouseController {
             return new ResponseEntity<>(response.errorMessage, response.status);
         }
         return new ResponseEntity<>(response.body, HttpStatus.OK);
-    }
-
-    // Get recall for an item
-    @RequestMapping(path = "/recalls/{itemId}", method = RequestMethod.GET)
-    public ResponseEntity<?> searchRecall(@PathVariable Integer itemId) {
-        Item item = items.findItemById(itemId);
-        return new ResponseEntity<>(RecallAPI.checkRecall(item), HttpStatus.OK);
     }
 }
